42537270f236d85fbcf87545913467677e3a96c1
Albert Albala <albert@mediatribe.net>
2023-01-13 21:39:04 -0500

    Optimize image, add rebuild script (#2)

--- test.sh
+++ test.sh
@@ -1,9 +1,6 @@
 set -e
-docker pull node
-docker pull dcycle/markdown-lint
+docker pull node:alpine
 docker build -t local-dcycle-markdown-lint-image .
 
-docker run -v $(pwd)/example:/app/code dcycle/markdown-lint --help
-docker run -v $(pwd)/example:/app/code dcycle/markdown-lint /app/code/markdown-file-no-errors.md
 docker run -v $(pwd)/example:/app/code local-dcycle-markdown-lint-image --help
 docker run -v $(pwd)/example:/app/code local-dcycle-markdown-lint-image /app/code/markdown-file-no-errors.md
